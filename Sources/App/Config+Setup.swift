090cd90562f49c48aa3301ce149101628f684458
Larry Damman <ldamman@nomadpool.com>
2017-05-26 01:46:23 -0400

    Set project up for mysql
    - Note: You will need to edit mysql.json with your own test database info to run the tests

--- Sources/App/Config+Setup.swift
+++ Sources/App/Config+Setup.swift
@@ -1,4 +1,5 @@
 import FluentProvider
+import MySQLProvider
 
 extension Config {
     public func setup() throws {
@@ -12,7 +13,8 @@ extension Config {
     
     /// Configure providers
     private func setupProviders() throws {
-        try addProvider(FluentProvider.Provider.self)
+		try addProvider(FluentProvider.Provider.self)
+		try addProvider(MySQLProvider.Provider.self)
     }
     
     /// Add all models that should have their
